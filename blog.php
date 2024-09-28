7fc6acbd223271a0a491976fc822b0266ad3b1c9
TechWolf24 <amelomanovic-developer@gmail.com>
2024-09-28 18:44:31 +0200

    gotov projekat

---
+++ blog.php
@@ -0,0 +1,12 @@
+<?php include 'template_parts/header.php'; ?>
+
+    <!-- heades -->
+    <header class="blogHeader position-relative">
+        <img class="w-100" src="img/blog-header.jpg" alt="">
+        <h1 class="position-absolute top-50 start-50 display-1 text-white py-3 px-5 translate-middle">Blog</h1>
+    </header>
+
+    <?php include 'template_parts/blog-part.php'; ?>
+
+
+<?php include 'template_parts/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include 'template_parts/header.php'; ?>
+
+<!-- main header -->
+<header class="mainHeader">
+    <img class="w-100" src="img/header.jpg" alt="">
+</header>
+
+<!-- baner -->
+<section class="baner container text-center p-5 text-white position-relative">
+    <h1>Najbolja solucija za rast vaseg bisnisa</h1>
+    <p class="lead">Kada želite promociju svog brenda i usluge onda je najsvrsishodnije rešenje da imate sve na jednom
+        mestu: facebook i google, reklame, kopirajt, SEO...</p>
+</section>
+
+<?php include 'template_parts/usluge-part.php'; ?>
+
+<?php include 'template_parts/footer.php'; ?>
